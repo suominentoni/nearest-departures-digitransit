1ebd3e6047a88f1f9e7137d57b52c950a9cbb08c
Toni Suominen <toni.suominen@reaktor.com>
2019-01-13 11:56:10 +0200

    Initial commit

---
+++ NearestDeparturesDigitransit/NearestDeparturesDigitransit.h
@@ -0,0 +1,19 @@
+//
+//  NearestDeparturesDigitransit.h
+//  NearestDeparturesDigitransit
+//
+//  Created by Toni Suominen on 11/01/2019.
+//  Copyright © 2019 Toni Suominen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NearestDeparturesDigitransit.
+FOUNDATION_EXPORT double NearestDeparturesDigitransitVersionNumber;
+
+//! Project version string for NearestDeparturesDigitransit.
+FOUNDATION_EXPORT const unsigned char NearestDeparturesDigitransitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NearestDeparturesDigitransit/PublicHeader.h>
+
+#import "ObjC.h"
