64f2b6340adba3d5998f6c81eed6cf15c05200c2
Toni Suominen <toni.suominen@reaktor.com>
2019-04-19 12:14:48 +0300

    Remove formattedDepartureTime, and last reference to UIKit

--- NearestDeparturesDigitransit.podspec
+++ NearestDeparturesDigitransit.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "NearestDeparturesDigitransit"
-  s.version      = "2.0.1"
+  s.version      = "3.0.0"
   s.summary      = "A Swift framework for fetching transit data from the Digitransit API"
 
   # This description is used to generate tags and improve search results.
--- NearestDeparturesDigitransit/Model/Departure.swift
+++ NearestDeparturesDigitransit/Model/Departure.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import UIKit
 
 public struct Departure {
     public let line: Line
@@ -43,27 +42,6 @@ public struct Departure {
             return "Metro"
         }
         return routeData["shortName"] as? String ?? "-"
-    }
-
-    public func formattedDepartureTime() -> NSAttributedString {
-        let scheduledTime = scheduledDepartureTime.toTime()
-        let realTime = realDepartureTime.toTime()
-
-        if(scheduledTime != realTime && abs(scheduledDepartureTime - realDepartureTime) >= 60) {
-            let realString = NSMutableAttributedString(string: realTime)
-            let space = NSAttributedString(string: " ")
-            let scheduledString = NSMutableAttributedString(
-                string: scheduledTime,
-                attributes: [
-                    NSAttributedString.Key.strikethroughStyle: 1,
-                    NSAttributedString.Key.strikethroughColor: UIColor.lightGray,
-                    NSAttributedString.Key.foregroundColor: UIColor.gray,
-                    NSAttributedString.Key.baselineOffset: 0])
-            scheduledString.append(space)
-            scheduledString.append(realString)
-            return scheduledString
-        }
-        return NSAttributedString(string: scheduledDepartureTime.toTime())
     }
 }
 
